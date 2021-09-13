636d55739528eb12455d9ca6ea063217d541cdf4
4um3n <rum3ngr0zdan0v98@gmail.com>
2021-09-13 23:09:00 +0300

    modified:   Python/Advanced/Advanced/Lists-Stacks-And-Queues/Exercises/balanced_parentheses.py

--- Python/Advanced/Advanced/Lists-Stacks-And-Queues/Exercises/balanced_parentheses.py
+++ Python/Advanced/Advanced/Lists-Stacks-And-Queues/Exercises/balanced_parentheses.py
@@ -2,27 +2,17 @@ from collections import deque
 
 parentheses_data = input()
 parentheses_dict = {"{": "}", "[": "]", "(": ")"}
-open_parentheses, closed_parentheses = [], []
-i = 0
-while i < len(parentheses_data):
-    while i < len(parentheses_data) and parentheses_data[i] in parentheses_dict.keys():
-        open_parentheses.append(parentheses_data[i])
-        i += 1
-    while i < len(parentheses_data) and parentheses_data[i] in parentheses_dict.values():
-        closed_parentheses.append(parentheses_data[i])
-        i += 1
+opening_indexes = deque()
+for i in range(len(parentheses_data)):
+    if parentheses_data[i] in parentheses_dict.keys():
+        opening_indexes.append(i)
+    elif parentheses_data[i] in parentheses_dict.values():
+        open_i = opening_indexes.pop() if opening_indexes else None
+        if open_i is None or parentheses_data[i] != parentheses_dict[parentheses_data[open_i]]:
+            print("NO")
+            exit()
 
-    closed_parentheses = closed_parentheses[::-1]
-    if len(open_parentheses) == len(closed_parentheses):
-        for ind in range(len(open_parentheses)):
-            if closed_parentheses[ind] != parentheses_dict[open_parentheses[ind]]:
-                print("NO")
-                exit()
-
-        open_parentheses.clear()
-        closed_parentheses.clear()
-    else:
-        print("NO")
-        exit()
-        
-print("YES")
+if opening_indexes:
+    print("NO")
+else:
+    print("YES")
