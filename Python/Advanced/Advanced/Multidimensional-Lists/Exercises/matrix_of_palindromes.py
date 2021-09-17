a5875a022a84b55a7ceef2fb19bdc5ac4668d589
Rumen Grozdanov <89039290+4um3n@users.noreply.github.com>
2021-09-18 00:52:24 +0300

    Update matrix_of_palindromes.py

--- Python/Advanced/Advanced/Multidimensional-Lists/Exercises/matrix_of_palindromes.py
+++ Python/Advanced/Advanced/Multidimensional-Lists/Exercises/matrix_of_palindromes.py
@@ -1,3 +1,3 @@
 rows, cols = [int(n) for n in input().split()]
-matrix = [[chr(fl) + chr(fl+x) + chr(fl) for x in range(cols)] for fl in range(ord('a'), ord('a') + rows)]
+matrix = [[chr(fl) + chr(fl+c) + chr(fl) for c in range(cols)] for fl in range(ord('a'), ord('a') + rows)]
 [print(' '.join(matrix[r])) for r in range(len(matrix))]
