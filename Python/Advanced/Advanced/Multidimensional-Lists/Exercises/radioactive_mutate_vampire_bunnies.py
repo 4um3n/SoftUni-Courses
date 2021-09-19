d5ac8385ee9ec0d2d758c3fdd4248d768546d744
4um3n <rum3ngr0zdan0v98@gmail.com>
2021-09-19 22:39:14 +0300

    modified:   Python/Advanced/Advanced/Multidimensional-Lists/Exercises/radioactive_mutate_vampire_bunnies.py

--- Python/Advanced/Advanced/Multidimensional-Lists/Exercises/radioactive_mutate_vampire_bunnies.py
+++ Python/Advanced/Advanced/Multidimensional-Lists/Exercises/radioactive_mutate_vampire_bunnies.py
@@ -1,57 +1,59 @@
 from collections import deque
 
 
-def find_player(grid):
-    for r in range(len(grid)):
-        for c in range(len(grid[r])):
-            if grid[r][c] == "P":
+def find_player(field):
+    for r in range(len(field)):
+        for c in range(len(field[r])):
+            if field[r][c] == "P":
                 return r, c
 
 
-def find_bunnies(grid):
-    bunnies = [(r, c) for r in range(len(grid)) for c in range(len(grid[r])) if grid[r][c] == "B"]
+def find_bunnies(field):
+    bunnies = [(r, c) for r in range(len(field)) for c in range(len(field[r])) if field[r][c] == "B"]
     return bunnies
 
 
-def spread_bunnies(bunnies, grid):
+def spread_bunnies(bunnies, field):
     for r, c in bunnies:
-        if r-1 in range(len(grid)):
-            grid[r-1][c] = "B"
-        if r+1 in range(len(grid)):
-            grid[r+1][c] = "B"
-        if c-1 in range(len(grid[0])):
-            grid[r][c-1] = "B"
-        if c+1 in range(len(grid[0])):
-            grid[r][c+1] = "B"
+        if r-1 in range(len(field)):
+            field[r-1][c] = "B"
+        if r+1 in range(len(field)):
+            field[r+1][c] = "B"
+        if c-1 in range(len(field[0])):
+            field[r][c-1] = "B"
+        if c+1 in range(len(field[0])):
+            field[r][c+1] = "B"
 
-    return grid
+    return field
+
+
+def play(field, commands):
+    while commands:
+        direction = commands.popleft()
+        r, c = find_player(field)
+        field[r][c] = "."
+        if direction == "U" and r - 1 in range(len(field)):
+            r -= 1
+        elif direction == "D" and r + 1 in range(len(field)):
+            r += 1
+        elif direction == "L" and c - 1 in range(len(field[0])):
+            c -= 1
+        elif direction == "R" and c + 1 in range(len(field[0])):
+            c += 1
+        else:
+            field = spread_bunnies(find_bunnies(field), field)
+            [print(''.join(field[r])) for r in range(len(field))]
+            return f"won: {r} {c}"
+
+        field = spread_bunnies(find_bunnies(field), field)
+        if field[r][c] == "B":
+            [print(''.join(field[r])) for r in range(len(field))]
+            return f"dead: {r} {c}"
+        else:
+            field[r][c] = "P"
 
 
 rows, cols = [int(n) for n in input().split()]
-field = [list(input()) for _ in range(rows)]
-commands = deque(input())
-while commands:
-    direction = commands.popleft()
-    row, col = find_player(field)
-    field[row][col] = "."
-    if direction == "U" and row-1 in range(len(field)):
-        row -= 1
-    elif direction == "D" and row+1 in range(len(field)):
-        row += 1
-    elif direction == "L" and col-1 in range(len(field[0])):
-        col -= 1
-    elif direction == "R" and col+1 in range(len(field[0])):
-        col += 1
-    else:
-        field = spread_bunnies(find_bunnies(field), field)
-        [print(''.join(field[r])) for r in range(len(field))]
-        print(f"won: {row} {col}")
-        exit()
-
-    field = spread_bunnies(find_bunnies(field), field)
-    if field[row][col] == "B":
-        [print(''.join(field[r])) for r in range(len(field))]
-        print(f"dead: {row} {col}")
-        exit()
-    
-    field[row][col] = "P"
+matrix = [list(input()) for _ in range(rows)]
+commands_data = deque(input())
+print(play(matrix, commands_data))
