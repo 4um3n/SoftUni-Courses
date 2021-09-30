133b134420dc81f04e2b10402d5e7b17eeecca03
4um3n <rum3ngr0zdan0v98@gmail.com>
2021-09-30 17:27:40 +0300

    modified:   Python/Advanced/Advanced/0.6-File-Handling/Lab/word_count.py

--- Python/Advanced/Advanced/0.6-File-Handling/Lab/word_count.py
+++ Python/Advanced/Advanced/0.6-File-Handling/Lab/word_count.py
@@ -2,30 +2,22 @@ import re
 from os import path
 
 
-def create_input_file(input_file):
-    with open(input_file, 'w') as file:
-        line = input()
-        while line:
-            file.write(f"{line}\n")
-            line = input()
-
-
 def find_existing_words(searched_words_file, input_file):
     with open(searched_words_file, 'r') as file:
-        searched_words = file.readline().split()
+        searched_words = file.read().split()
 
-    words_dict = {}
+    occurrences = {}
     for word in searched_words:
-        pattern = re.compile(fr"^|(?<=[^A-Za-z\d])({word})", re.IGNORECASE)
-        if word not in words_dict:
-            words_dict[word] = 0
+        pattern = re.compile(fr"^|(?<=[^A-Za-z\d])({word})(?=[^A-Za-z\d])|$", re.IGNORECASE)
+        if word not in occurrences:
+            occurrences[word] = 0
 
         with open(input_file, 'r') as file:
             for line in file:
                 found_words = [w for w in re.findall(pattern, line) if w]
-                words_dict[word] += len(found_words)
+                occurrences[word] += len(found_words)
 
-    return words_dict
+    return occurrences
 
 
 def create_output_file(output_file, words):
@@ -44,7 +36,6 @@ input_file_path = path.join(current_dir_path, 'Files', 'input.txt')
 searched_words_file_path = path.join(current_dir_path, 'Files', 'words.txt')
 output_file_path = path.join(current_dir_path, 'Files', 'output.txt')
 
-create_input_file(input_file_path)
 words_data = find_existing_words(searched_words_file_path, input_file_path)
 create_output_file(output_file_path, words_data)
 print(read_output_file(output_file_path))
