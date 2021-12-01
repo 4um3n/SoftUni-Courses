85d1bb7fcc3f530ef8c1bdb314da6cc568f5b0a2
4um3n <rum3ngr0zdan0v98@gmail.com>
2021-12-01 19:00:24 +0200

    modified:   Python/0.4-OOP/Exams/22-August-2020/Tests/project/test/test.py

--- Python/0.4-OOP/Exams/22-August-2020/Tests/project/test/test.py
+++ Python/0.4-OOP/Exams/22-August-2020/Tests/project/test/test.py
@@ -11,7 +11,7 @@ class StudentReportCardTests(TestCase):
         self.assertEqual(12, self.student_card.school_year)
         self.assertDictEqual({}, self.student_card.grades_by_subject)
 
-    def test_init_student_name_cannot_be_empty_string_raises(self):
+    def test_student_name_cannot_be_empty_string_raises(self):
         with self.assertRaises(ValueError) as ex:
             StudentReportCard("", 12)
         self.assertEqual(f"Student Name cannot be an empty string!", str(ex.exception))
@@ -19,7 +19,7 @@ class StudentReportCardTests(TestCase):
         self.student_card.student_name = "new_name"
         self.assertEqual("new_name", self.student_card.student_name)
 
-    def test_init_school_year_must_be_between_1_and_12_raises(self):
+    def test_school_year_not_in_range_1_to_12_raises(self):
         with self.assertRaises(ValueError) as ex:
             StudentReportCard("student", 0)
         self.assertEqual(f"School Year must be between 1 and 12!", str(ex.exception))
@@ -27,35 +27,33 @@ class StudentReportCardTests(TestCase):
         self.student_card.school_year = 1
         self.assertEqual(1, self.student_card.school_year)
 
-    def test_add_grade_subject_not_in_grades_by_subjects(self):
+    def test_add_grade(self):
         self.assertDictEqual({}, self.student_card.grades_by_subject)
         result = self.student_card.add_grade("subject", 5)
         self.assertDictEqual({"subject": [5]}, self.student_card.grades_by_subject)
         self.assertIsNone(result)
 
-        self.student_card.add_grade("subject1", 5)
-        expected = {"subject": [5], "subject1": [5]}
-        self.assertDictEqual(expected, self.student_card.grades_by_subject)
-
-    def test_add_grade_subject_in_grades_by_subjects(self):
         self.student_card.grades_by_subject = {"subject": [5]}
         self.student_card.add_grade("subject", 6)
         expected = {"subject": [5, 6]}
         self.assertDictEqual(expected, self.student_card.grades_by_subject)
 
-    def test_report_for_average_grade_by_subject_no_subjects_found(self):
+        self.student_card.add_grade("subject1", 5)
+        expected = {"subject": [5, 6], "subject1": [5]}
+        self.assertDictEqual(expected, self.student_card.grades_by_subject)
+
+    def test_report_for_average_grade_by_subject(self):
         self.assertDictEqual({}, self.student_card.grades_by_subject)
         result = self.student_card.average_grade_by_subject()
         self.assertEqual("", result)
 
-    def test_report_for_average_grade_by_subject(self):
         self.student_card.grades_by_subject = {"subject": [5, 6], "subject1": [6, 6]}
         expected = f"subject: 5.50\n" \
                    f"subject1: 6.00"
         result = self.student_card.average_grade_by_subject()
         self.assertEqual(expected, result)
 
-    def test_report_average_grade_for_all_subjects_success(self):
+    def test_report_average_grade_for_all_subjects(self):
         self.student_card.grades_by_subject = {"subject": [5, 5], "subject1": [6, 6]}
         expected = f"Average Grade: 5.50"
         result = self.student_card.average_grade_for_all_subjects()
