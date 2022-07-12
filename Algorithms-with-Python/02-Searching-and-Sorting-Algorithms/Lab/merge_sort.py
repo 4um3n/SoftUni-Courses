84a71773c91cf1c2ff62684d3de6e951073254f2
spica <rum3ngr0zdan0v98@gmail.com>
2022-07-12 13:37:23 +0300

    modified:   Algorithms-with-Python/02-Searching-and-Sorting-Algorithms/Lab/merge_sort.py

--- Algorithms-with-Python/02-Searching-and-Sorting-Algorithms/Lab/merge_sort.py
+++ Algorithms-with-Python/02-Searching-and-Sorting-Algorithms/Lab/merge_sort.py
@@ -2,30 +2,25 @@ import time
 
 
 def merge_arrays(left_array, right_array):
-    result = [None] * (len(left_array) + len(right_array))
+    result = []
     left_ind = 0
     right_ind = 0
-    result_ind = 0
 
     while left_ind < len(left_array) and right_ind < len(right_array):
         if left_array[left_ind] < right_array[right_ind]:
-            result[result_ind] = left_array[left_ind]
+            result.append(left_array[left_ind])
             left_ind += 1
         else:
-            result[result_ind] = right_array[right_ind]
+            result.append(right_array[right_ind])
             right_ind += 1
 
-        result_ind += 1
-
     while left_ind < len(left_array):
-        result[result_ind] = left_array[left_ind]
+        result.append(left_array[left_ind])
         left_ind += 1
-        result_ind += 1
 
     while right_ind < len(right_array):
-        result[result_ind] = right_array[right_ind]
+        result.append(right_array[right_ind])
         right_ind += 1
-        result_ind += 1
 
     return result
 
